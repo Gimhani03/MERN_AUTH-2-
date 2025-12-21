4e846fe73c21d714bc3ec77bb48daf53b17fbc27
Gimhani <silvagsd@students.nsbm.ac.lk>
2025-12-21 23:55:07 +0530

    Add App.jsx as main application component with routing setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './pages/login.jsx'
+import Register from './pages/register.jsx'
+import { Route, Routes } from 'react-router-dom'
+import ForgotPassword from './pages/ForgotPassword.jsx'
+import ResetPassword from './pages/ResetPassword.jsx'
+import ConfirmPassword from './pages/ConfirmPassword.jsx'
+import Dashboard from './pages/dashboard.jsx'
+
+function App() {
+  
+
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/confirm-password" element={<ConfirmPassword />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  )
+}
+
+export default App
